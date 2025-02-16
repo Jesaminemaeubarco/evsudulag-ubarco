db2ff0c26bf636a97fc86def5561a32eb87bcd85
jesamine <ubarcoayessamae080710@gmail.com>
2025-02-16 20:47:09 +0800

    adding all files

---
+++ samine/src/app/main/users.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoutesModule } from './routes.module';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms'
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    RoutesModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class UsersModule { }
